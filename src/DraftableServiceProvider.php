368987dae0d4dd0b06bf9b7306deac4b60280993
David Couto <davidcouto@gmail.com>
2024-01-22 12:57:38 +0000

    Fixed error

--- src/DraftableServiceProvider.php
+++ src/DraftableServiceProvider.php
@@ -12,7 +12,7 @@ class DraftableServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        $this->mergeConfigFrom(__DIR__ . '/../config', 'laravel-draftable');
+        $this->mergeConfigFrom(__DIR__ . '/config', 'laravel-draftable');
     }
 
     /**
